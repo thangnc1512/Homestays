6490793599283bf56a4b83c4bd07001f5daa0499
ThangNguyen <>
2019-10-11 08:45:48 +0700

    add Fragment + Screen Register

---
+++ app/src/main/java/com/tnt/homestays/fragment/FavouriteFragment.java
@@ -0,0 +1,7 @@
+package com.tnt.homestays.fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class FavouriteFragment extends Fragment {
+    
+}
---
+++ app/src/main/java/com/tnt/homestays/fragment/HomeFragment.java
@@ -0,0 +1,7 @@
+package com.tnt.homestays.fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+}
